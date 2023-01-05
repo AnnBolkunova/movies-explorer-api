e0e82b66ba9867c9bb526e0c940bea85ddc8d670
AnnBolkunova <105433038+AnnBolkunova@users.noreply.github.com>
2023-01-05 23:27:53 +0300

    Delete CommonError.js

--- errors/CommonError.js
+++
@@ -1,8 +0,0 @@
-const { constants } = require('http2');
-
-module.exports.CommonError = (err, req, res, next) => {
-    const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
-    const message = err.message || 'Неизвестная ошибка';
-    res.status(status).send({ message });
-    next();
-};
