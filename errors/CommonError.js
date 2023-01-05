b5a1f217b517d5929ad3ee9136596fb09963ccc2
AnnBolkunova <105433038+AnnBolkunova@users.noreply.github.com>
2023-01-05 22:53:37 +0300

    Add files via upload

--- errors/CommonError.js
+++ errors/CommonError.js
@@ -1,3 +1,5 @@
+const { constants } = require('http2');
+
 module.exports.CommonError = (err, req, res, next) => {
     const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
     const message = err.message || 'Неизвестная ошибка';
