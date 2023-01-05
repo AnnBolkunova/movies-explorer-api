da72db0a5398f5a9a4e06d7fb08c1b146c01cc86
AnnBolkunova <105433038+AnnBolkunova@users.noreply.github.com>
2023-01-05 23:01:39 +0300

    Revert "Level 1"

--- errors/CommonError.js
+++ errors/CommonError.js
@@ -1,5 +1,3 @@
-const { constants } = require('http2');
-
 module.exports.CommonError = (err, req, res, next) => {
     const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
     const message = err.message || 'Неизвестная ошибка';
